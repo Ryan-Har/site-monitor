0df1a093b7382f7187cddaf38b75bb8bbae76788
ryan <ryan@harrisit.co.uk>
2024-05-30 10:15:27 +0100

    chore: Refactor code to use environment variable fallback in getEnvWithDefault function

--- src/config/config.go
+++ src/config/config.go
@@ -50,3 +50,11 @@ func (c *Config) FirebaseConfigAsJsonString() string {
 	jsonData := c.FirebaseConfigAsJsonBytes()
 	return string(jsonData)
 }
+
+func getEnvWithDefault(key string, fallback string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return fallback
+	}
+	return value
+}
